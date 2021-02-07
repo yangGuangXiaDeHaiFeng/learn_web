ef1a831c87bd9c63173bf8ec236d817c2d101a63
wangjiao <jiaowang01@wisedu.com>
2021-02-07 18:02:51 +0800

    【add】Class.getResource(path)和Class.getClassLoader().getResource(path)

--- javaDemo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ javaDemo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		learnPath();
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	private static void learnPath() {
+		System.out.print("当前类编译后所在的包：");
+		System.out.println(DemoApplication.class.getResource(""));
+		System.out.print("当前类编译后所在的classPath：");
+		System.out.println(DemoApplication.class.getResource("/"));
+
+		System.out.print("当前类编译后所在的classPath");
+		System.out.println(DemoApplication.class.getClassLoader().getResource(""));
+		System.out.print("不能这么用：");
+		System.out.println(DemoApplication.class.getClassLoader().getResource("/"));
+
 	}
 
 }
